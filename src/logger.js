39e00e513d0b8a43a0a0479b1d3f4b74fadf2ad1
Farid Hartono Gunawan <farid.gunawan@finnsbeachclub.com>
2026-08-24 14:46:49 +0800

    Fix: change logger timestamp to GMT+8 time

--- src/logger.js
+++ src/logger.js
@@ -2,7 +2,10 @@ const originalLog = console.log.bind(console);
 const originalWarn = console.warn.bind(console);
 const originalError = console.error.bind(console);
 
-const timestamp = () => new Date().toISOString();
+const timestamp = () => {
+    const gmt8 = new Date(Date.now() + 8 * 60 * 60 * 1000);
+    return gmt8.toISOString().replace('Z', '+08:00');
+};
 
 console.log = (...args) => originalLog(`[${timestamp()}]`, ...args);
 console.warn = (...args) => originalWarn(`[${timestamp()}]`, ...args);
